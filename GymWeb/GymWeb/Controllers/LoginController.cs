72aff38c88cabdfea4cb3bc9f4711a6f729f650b
Eduardo <74618883+eduardo-1898@users.noreply.github.com>
2023-09-25 20:26:02 -0600

    Carga de proyecto inicial
    Se incluye el desarrollo con el template a utilizar en el proyecto para que sirva de base

---
+++ GymWeb/GymWeb/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        public IActionResult RecuperarContrasenna()
+        {
+            return View();
+        }
+
+
+    }
+}
